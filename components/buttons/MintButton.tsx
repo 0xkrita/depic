b422030dbcadae05a90903551693344d1c093831
krita <108590406+0xkrita@users.noreply.github.com>
2022-08-07 00:08:45 -0700

    feat: add edition contracts

--- components/buttons/MintButton.tsx
+++ components/buttons/MintButton.tsx
@@ -1,7 +1,9 @@
+import { BigNumber } from 'ethers';
 import { useState } from 'react';
 import styled from 'styled-components';
+import { usePrepareSendTransaction, useSendTransaction } from 'wagmi';
+import { client } from '../../utils/storage';
 import SvgMintButton from '../icons/buttons/MintButton';
-import MintModal from '../MintModal';
 
 const MintButtonSvg = styled.svg`
   position: absolute;
@@ -17,20 +19,77 @@ export default function MintButton() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const mint = async () => {
+    const imageOriginUrl =
+      'https://user-images.githubusercontent.com/87873179/144324736-3f09a98e-f5aa-4199-a874-13583bf31951.jpg';
+    const image = await fetch(imageOriginUrl)
+      .then((r) => r.blob())
+      .catch((r) => {
+        if (!r.ok) {
+          throw new Error(
+            `error fetching image: [${r.statusCode}]: ${r.status}`
+          );
+        }
+        return r.blob();
+      });
+    const sampleNFT = {
+      image, // use image Blob as `image` field
+      name: "Storing the World's Most Valuable Virtual Assets with NFT.Storage",
+      description: 'The metaverse is here. Where is it all being stored?',
+      properties: {
+        type: 'blog-post',
+        origins: {
+          http: 'https://nft.storage/blog/post/2021-11-30-hello-world-nft-storage/',
+          ipfs: 'ipfs://bafybeieh4gpvatp32iqaacs6xqxqitla4drrkyyzq6dshqqsilkk3fqmti/blog/post/2021-11-30-hello-world-nft-storage/',
+        },
+        authors: [{ name: 'David Choi' }],
+        content: {
+          'text/markdown':
+            'The last year has witnessed the explosion of NFTs onto the world’s mainstage. From fine art to collectibles to music and media, NFTs are quickly demonstrating just how quickly grassroots Web3 communities can grow, and perhaps how much closer we are to mass adoption than we may have previously thought. <... remaining content omitted ...>',
+        },
+      },
+    };
+
+    const metadata = await client.store(sampleNFT);
+
+    alert(metadata);
+
+    const { config } = usePrepareSendTransaction({
+      request: { to: 'moxey.eth', value: BigNumber.from('10000000000000000') },
+    });
+    const { data, isLoading, isSuccess, sendTransaction } =
+      useSendTransaction(config);
+
+    return (
+      <div>
+        <button disabled={!sendTransaction} onClick={() => sendTransaction?.()}>
+          Send Transaction
+        </button>
+        {isLoading && <div>Check Wallet</div>}
+        {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+      </div>
+    );
+  };
+
   return (
     <>
-      <button type="button" onClick={handleShow}>
+      <button type="button" onClick={mint}>
         <MintButtonSvg>
           <SvgMintButton></SvgMintButton>
         </MintButtonSvg>
       </button>
 
+      {/* <button type="button" onClick={handleShow}>
+        <MintButtonSvg>
+          <SvgMintButton></SvgMintButton>
+        </MintButtonSvg>
+      </button>
       {show && (
         <>
           <MintModal></MintModal>
           <button onClick={handleClose}>[x]</button>
         </>
-      )}
+      )} */}
     </>
   );
 }
