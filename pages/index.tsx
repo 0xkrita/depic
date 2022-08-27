1a6f630e347c0771096f04d80a91aaa2b984f329
krita <108590406+0xkrita@users.noreply.github.com>
2022-08-26 17:40:04 -0700

    feat: update favicon and add test meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,10 @@ const Home: NextPage = () => {
       <Head>
         <title>DEPIC</title>
         <meta name="description" content="A Hackathon Project" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎨</text></svg>"
+        />
       </Head>
       <div>
         <Image
